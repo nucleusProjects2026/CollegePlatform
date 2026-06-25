4e34da23c243ee7b6c6896365af1d4ef67040d38
Sunil <sunillamani817@gmail.com>
2026-06-25 19:47:53 +0530

    fied Route path bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import FacultyLocator from './pages/FacultyLocator'
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/faculty-locator" replace />} />
-      <Route path="/faculty-locator" element={<FacultyLocator />} />
+      <Route
+        path="/faculty-locator"
+        element={<FacultyLocator />}
+      />
     </Routes>
   )
 }
